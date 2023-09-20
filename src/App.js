b48445c9e4b3748c50761c92403f07cf0498757e
caspermike997 <caspermike97@gmail.com>
2023-09-20 15:06:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from './components/Cards'
+import Header from './components/Header'
+import Navbar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+    <div className='App'>
+      <Card />
+      <Header />
+      <Navbar />
+
+    </div>
+    </>
+  )
+}
+
+export default App
